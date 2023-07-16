d17a3f150d4f7d3e9c95c78c42109c3b97797eeb
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-07-16 01:27:42 +0000

    feat: fix navigation and button area

--- src/app/ButtonArea.tsx
+++ src/app/ButtonArea.tsx
@@ -1,7 +1,33 @@
+'use client';
+
+import { LiaInstagram } from 'react-icons/lia';
+
+import { useRouter } from 'next/navigation';
+
 export default function Navigation({ className }: { className: string }) {
+  const router = useRouter();
+  const buttonItems = [
+    { id: 'instagram', icon: LiaInstagram, href: 'https://www.instagram.com' },
+    { id: 'instagram', icon: LiaInstagram, href: 'https://www.instagram.com' },
+    { id: 'instagram', icon: LiaInstagram, href: 'https://www.instagram.com' },
+    { id: 'instagram', icon: LiaInstagram, href: 'https://www.instagram.com' },
+    { id: 'instagram', icon: LiaInstagram, href: 'https://www.instagram.com' },
+  ];
   return (
-    <div className={`${className} flex flex-col text-center`}>
-      <div className="fixed">button area dayo</div>
+    <div className={`${className}`}>
+      <div className="fixed flex flex-col items-center">
+        <div>button area dayo</div>
+        <div className="text-center">
+          {buttonItems.map((item) => (
+            <item.icon
+              key={item.id}
+              onClick={() => {
+                router.push(item.href);
+              }}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -1,45 +1,25 @@
 'use client';
 
 import Link from 'next/link';
-import { Navbar } from '@nextui-org/react';
-import styles from './Navigation.module.css';
 
 export default function Navigation({ className }: { className: string }) {
   const navItems = [
-    'ABOUT',
-    'ALL',
-    'WEB',
-    'IDENTITY',
-    'DM / CARD',
-    'EDITORIAL',
-    'OTHER',
+    { id: 'ABOUT', href: '/about' },
+    { id: 'ALL', href: '/all' },
+    { id: 'WEB', href: '/web' },
+    { id: 'IDENTITY', href: '/identity' },
+    { id: 'DM / CARD', href: '/dm-card' },
+    { id: 'EDITORIAL', href: '/editorial' },
+    { id: 'OTHER', href: 'other' },
   ];
   return (
-    <div className={`${styles.navigation} ${className} p-2`}>
+    <div className={`${className} p-2`}>
       <div className="fixed">
-        <Navbar disableBlur containerCss={{ backgroundColor: 'transparent' }}>
-          <Navbar.Brand>
-            <Navbar.Toggle aria-label="toggle navigation" showIn="sm" />
-          </Navbar.Brand>
-          <Navbar.Content
-            hideIn="sm"
-            variant="underline"
-            css={{ flexDirection: 'column' }}
-          >
-            {navItems.map((item) => (
-              <Navbar.Link href="#" key={item}>
-                {item}
-              </Navbar.Link>
-            ))}
-          </Navbar.Content>
-          <Navbar.Collapse>
-            {navItems.map((item) => (
-              <Navbar.CollapseItem key={item}>
-                <Link href="/">{item}</Link>
-              </Navbar.CollapseItem>
-            ))}
-          </Navbar.Collapse>
-        </Navbar>
+        {navItems.map((item) => (
+          <div key={item.id}>
+            <Link href={item.href}>{item.id}</Link>
+          </div>
+        ))}
       </div>
     </div>
   );
