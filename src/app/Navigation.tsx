65c497573eeb4e120d9dec0919c09f39a8ff4ff3
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-07-19 11:00:48 +0000

    fix: navigation area about button

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -1,7 +1,11 @@
 'use client';
 
+import clsx from 'clsx';
 import Link from 'next/link';
+import { IoIosArrowDown } from 'react-icons/io';
+
 import useStore from '../../store';
+import styles from './Navigation.module.scss';
 
 export default function Navigation({ className }: { className: string }) {
   const navItems = [
@@ -14,17 +18,22 @@ export default function Navigation({ className }: { className: string }) {
   ];
   const toggle = useStore((state) => state.toggle);
   return (
-    <div className={`${className}`}>
+    <div className={clsx(styles.navigation, className)}>
       <div className="fixed flex flex-col justify-between h-full w-1/12">
         <div className="p-4">
           <button type="submit" onClick={toggle}>
-            ABOUT
+            <span className="flex flex-row items-center justify-between">
+              ABOUT
+              <IoIosArrowDown className="mx-2" />
+            </span>
           </button>
         </div>
         <div className="p-4">
           {navItems.map((item) => (
             <div key={item.id} className="py-2">
-              <Link href={item.href}>{item.id}</Link>
+              <Link href={item.href} className="text-inherit">
+                {item.id}
+              </Link>
             </div>
           ))}
         </div>
