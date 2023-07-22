17b2a3088cd5f89728d27f6d19171ec9608fbb69
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-07-22 06:15:31 +0000

    feat: fix content container component

--- src/app/components/elements/ContentContainer/ContentContainer.tsx
+++ src/app/components/elements/ContentContainer/ContentContainer.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image';
+'use client';
 
-// import styles from './Content.module.css';
+import Image from 'next/image';
+import clsx from 'clsx';
+import Rating from 'react-star-rating-component';
+
+import styles from './Content.module.css';
 
 type ContentContainerProps = {
   title: string;
@@ -10,19 +14,45 @@ type ContentContainerProps = {
 export type ContentContainerItem = {
   id: string;
   iconUrl: string;
+  lank: number;
+  description: string;
 };
 
 export const ContentContainer = (props: ContentContainerProps) => {
   const { title, items } = props;
+  const iconImageSize = 80;
 
   return (
     <div className="flex flex-col text-right mt-8 mb-28">
       <h1 className="mb-12 text-center">{title}</h1>
 
-      <div className="grid grid-cols-4 gap-16">
+      <div className="grid grid-cols-2 gap-16">
         {items.map((item) => (
-          <div key={item.id} className="relative w-full h-24">
-            <Image src={item.iconUrl} alt={item.id} fill />
+          <div key={item.id} className="relative w-full flex flex-col">
+            <div className="flex flex-row justify-left items-center">
+              <Image
+                src={item.iconUrl}
+                alt={item.id}
+                width={iconImageSize}
+                height={iconImageSize}
+                className="mx-8"
+              />
+              <div className="flex flex-col h-full justify-around">
+                <span className="text-left">{item.id}</span>
+                <Rating value={item.lank} emptyStarColor="#ccc" />
+              </div>
+            </div>
+            <div
+              className={clsx(
+                styles.description,
+                'w-full',
+                'mx-8',
+                'mt-4',
+                'text-left',
+              )}
+            >
+              {item.description}
+            </div>
           </div>
         ))}
       </div>
--- src/app/skillset/page.tsx
+++ src/app/skillset/page.tsx
@@ -6,59 +6,130 @@ import {
 export default function Skillset() {
   const languageAndFramework: ContentContainerItem[] = [
     {
+      id: 'ShellScript',
+      iconUrl:
+        'https://github.com/devicons/devicon/raw/master/icons/bash/bash-original.svg',
+      lank: 5,
+      description:
+        '複雑なコードをスクラッチから書くことができます。簡単な業務効率化ツールやユーティリティなどプロジェクトの課題や状況を判断して、適切なプログラムを作成することができます。',
+    },
+    {
       id: 'java',
       iconUrl:
         'https://github.com/devicons/devicon/raw/master/icons/java/java-original-wordmark.svg',
+      lank: 5,
+      description:
+        '複雑なコードをスクラッチから書くことができます。JVM含めた仮想マシン層の基本原理を理解しており、トラブルシューティングへの対応も可能です。',
     },
     {
       id: 'SpringBoot',
       iconUrl:
         'https://github.com/devicons/devicon/raw/master/icons/spring/spring-original-wordmark.svg',
+      lank: 4,
+      description:
+        'webfluxなど応用的な実装が可能です。DIコンテナの仕組みや基本原理を理解しており既存のコードに対してより良くリファクタリングすることも可能です',
     },
     {
       id: 'javascript',
       iconUrl:
         'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
+      lank: 3,
+      description:
+        'あまり複雑なコードをスクラッチで書くことはできませんが、ライブラリの実装や基本的なコードは問題なく書けます。',
     },
     {
       id: 'Node.js',
       iconUrl:
         'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg',
+      lank: 3,
+      description:
+        'expressなどのライブラリを使ったAPIサーバーやその他基本的なサーバーサイドの実装ができます。',
     },
     {
       id: 'Vue.js',
       iconUrl:
         'https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg',
+      lank: 2,
+      description:
+        'コンポーネントの基本原理を理解しており、vuexやvue-roterなどのライブラリを利用したWebアプリケーションを作成することができます。',
     },
     {
       id: 'css',
       iconUrl:
         'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg',
+      lank: 2,
+      description:
+        '各プロパティの基本的な機能や特性を理解しており、デザイン通りの実装を行うことができます。',
     },
     {
       id: 'html',
       iconUrl:
         'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg',
+      lank: 3,
+      description:
+        '各種タグの基本的な機能や特性を理解しており、デザイン通りの実装を行うことができます。',
     },
     {
       id: 'React',
       iconUrl:
         'https://github.com/devicons/devicon/raw/master/icons/react/react-original-wordmark.svg',
+      lank: 3,
+      description:
+        'コンポーネントやフックの基本原理を理解しており、reduxやzustand、react-routerなどのライブラリを利用したWebアプリケーションを作成することができます。',
     },
     {
       id: 'Next.js',
       iconUrl:
         'https://github.com/devicons/devicon/raw/master/icons/nextjs/nextjs-original-wordmark.svg',
+      lank: 2,
+      description:
+        'サーバーコンポーネント/クライアントコンポーネントやSSG/SSRの仕組み・特徴を理解しており、一般的なWebアプリケーションを作成することができます。このポートフォリオサイトもNext.jsを使って作成しております。',
     },
     {
       id: 'Python',
       iconUrl:
         'https://github.com/devicons/devicon/raw/master/icons/python/python-original-wordmark.svg',
+      lank: 3,
+      description:
+        'あまり複雑なコードをスクラッチで書くことはできませんが、モジュールの実装や基本的なコードは問題なく書けます。',
     },
     {
       id: 'Django',
       iconUrl:
         'https://github.com/devicons/devicon/raw/master/icons/django/django-plain-wordmark.svg',
+      lank: 2,
+      description:
+        'フレームワークの基本的な機能や特徴を理解しており、APIサーバー用のライブラリを使用した基本的な実装ができます。',
+    },
+  ];
+  const developementTool: ContentContainerItem[] = [
+    {
+      id: 'ShellScript',
+      iconUrl:
+        'https://github.com/devicons/devicon/raw/master/icons/bash/bash-original.svg',
+      lank: 3,
+      description:
+        'あまり複雑なコードをスクラッチで書くことはできませんが、ライブラリの実装や基本的なコードは問題なく書けます。',
+    },
+  ];
+  const infrastructure: ContentContainerItem[] = [
+    {
+      id: 'ShellScript',
+      iconUrl:
+        'https://github.com/devicons/devicon/raw/master/icons/bash/bash-original.svg',
+      lank: 3,
+      description:
+        'あまり複雑なコードをスクラッチで書くことはできませんが、ライブラリの実装や基本的なコードは問題なく書けます。',
+    },
+  ];
+  const other: ContentContainerItem[] = [
+    {
+      id: 'ShellScript',
+      iconUrl:
+        'https://github.com/devicons/devicon/raw/master/icons/bash/bash-original.svg',
+      lank: 3,
+      description:
+        'あまり複雑なコードをスクラッチで書くことはできませんが、ライブラリの実装や基本的なコードは問題なく書けます。',
     },
   ];
   return (
@@ -67,9 +138,9 @@ export default function Skillset() {
         title="Language & Framework"
         items={languageAndFramework}
       />
-      <ContentContainer title="Development Tool" items={languageAndFramework} />
-      <ContentContainer title="Infrastructure" items={languageAndFramework} />
-      <ContentContainer title="Other" items={languageAndFramework} />
+      <ContentContainer title="Development Tool" items={developementTool} />
+      <ContentContainer title="Infrastructure" items={infrastructure} />
+      <ContentContainer title="Other" items={other} />
     </div>
   );
 }
