c85e7d1b62a7218a2ba6cc2729fd5337e593bae1
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-07-25 01:53:58 +0000

    refactor: add loading and error for root page

---
+++ src/app/components/elements/Spinner/Spinner.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+
+export const Spinner = ({ color = 'border-blue-500' }) => (
+  <div className="my-16 flex justify-center">
+    <div
+      className={clsx(
+        'h-10',
+        'w-10',
+        'animate-spin',
+        'rouded-full',
+        'border-4',
+        color,
+        'border-t-transparent',
+      )}
+    />
+  </div>
+);
---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import { Spinner } from './components/elements/Spinner';
+
+export default function loading() {
+  return <Spinner />;
+}
