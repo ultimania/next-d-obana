1d972643d997a1ea4f8fc499c6cb20b0bd944fa7
Takumi Fukaya <fukaya-takumi@sysj.co.jp>
2023-08-06 03:19:49 +0000

    feat: add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,23 @@
+import { Noto_Serif_JP } from 'next/font/google';
+import clsx from 'clsx';
+import Link from 'next/link';
+
+const notojp = Noto_Serif_JP({
+  weight: ['400', '500'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div
+      className={clsx('h-screen', 'relative', notojp.className, 'bg-slate-50')}
+    >
+      <div className={clsx('top-1/3', 'relative')}>
+        <h1>Page Not Found</h1>
+        <h2>- 404 -</h2>
+        <Link href="/">Go Home</Link>
+      </div>
+    </div>
+  );
 }
