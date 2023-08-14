c7dd0acb72fb8a94215e48294616c5ba5db86f77
ultimania <fukaya-takumi@sysj.co.jp>
2023-08-14 01:16:15 +0000

    feat: fix node version for actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const branchName = '/main';
+
 const nextConfig = {
   distDir: 'build',
+  reactStrictMode: true,
+  assetPrefix: branchName,
+  basePath: branchName,
 };
 
 module.exports = nextConfig;
